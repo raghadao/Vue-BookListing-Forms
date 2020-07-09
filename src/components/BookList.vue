e4376c0ec3266e78cda48d54b8dd54272969e7a4
raghadao <67149416+raghadao@users.noreply.github.com>
2020-07-09 03:50:47 +0300

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -60,7 +60,7 @@ export default {
   },
   computed:{
    filteredBooks() {
-     return _.filter(this.book, ["ownership", this.holding])
+     return _.filter(this.books, ["ownership", this.holding])
    }
   },
   methods: {
