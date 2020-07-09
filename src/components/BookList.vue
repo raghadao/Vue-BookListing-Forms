31bec7a5a2143bdbdd5e339591cff8ab35d74e42
raghadao <67149416+raghadao@users.noreply.github.com>
2020-07-09 04:00:50 +0300

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input type="text" placeholder="Search Books" v-model="searchInput">
     <ul class="exists">
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books by Ownership</h2>
@@ -35,7 +36,8 @@ export default {
           title: "Self-Reliance", 
           author: "Ralph Waldo Emerson", 
           finishedReading: true,
-          ownership: "borrowed"
+          ownership: "borrowed",
+          searchInput:""
            },
         { 
           title: "American Gods",
@@ -61,7 +63,16 @@ export default {
   computed:{
    filteredBooks() {
      return _.filter(this.books, ["ownership", this.holding])
+   },
+   searchedBooks(){
+const searchFilter = book => {
+  return book.title.toLowerCase().match(this.searchInput.toLowerCase())
+
+};
+ return _.filter(this.books, searchFilter);
+
    }
+
   },
   methods: {
     appendBook(bookData) {
